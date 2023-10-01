d3c4a3231b6c9949c7bb89858eb0da9b02e22c9b
Mardzy <mardlingp@gmail.com>
2023-10-01 19:12:50 +0200

    remove unneccessary files

--- dist/entities/index.js
+++
@@ -1,3 +0,0 @@
-export * from './base.entity';
-export * from './card.entity';
-export * from './product.entity';
--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { addProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from 'controllers';
-var router = Router();
-router.post('/products', addProduct);
-router.get('/products', getAllProducts);
-router.get('/products/:id', getProductById);
-router.patch('/products/:id', updateProduct);
-router.delete('/products/:id', deleteProduct);
-export { router };
